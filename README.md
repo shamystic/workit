8e476242d921e735c8a70199f19eed969ed6c382
shamystic <ssh50@duke.edu>
2018-11-07 02:02:30 -0500

    updated description

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # workit
 Workout application
+
+
--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from jinja2 import Template
-from flask import Flask, render_template
+from flask import Flask, request, render_template, url_for, session, redirect, jsonify, flash, Response, current_app
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
-
-
+    return render_template('main.html')
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
