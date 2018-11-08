84e47a717edeae5b15f732eaf82536e2d7d8fd57
Hemanth Yakkali <hy115@duke.edu>
2018-11-07 23:30:17 -0500

    Added equipments.html

--- app.py
+++ app.py
@@ -1,9 +1,47 @@
 from flask import Flask, request, render_template, url_for, session, redirect, jsonify, flash, Response, current_app
 from flask_sqlalchemy import SQLAlchemy
+# from models import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    email = db.Column(db.String(120), primary_key = True, nullable=False)
+    name = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"User '{self.name}', '{self.email}'"
+
+class Equipment(db.Model):
+    name = db.Column(db.String(120), primary_key = True)
+
+class Exercise(db.Model):
+    name = db.Column(db.String(120), primary_key = True)
+    body_part = db.Column(db.String(120))
+
+class Workout_Circuit(db.Model):
+    circuit_id = db.Column(db.Integer, primary_key = True)
+    workout_type = db.Column(db.String(120))
+
+class Fitness_Class(db.Model):
+    name = db.Column(db.String(120), primary_key = True)
+    location = db.Column(db.String(120), primary_key = True)
+
+# class ownsWorkout(db.Model):
+#     email = db.Column(db.String(120), db.ForeignKey(User.email))
+#     circuit_id = db.Column(db.Integer, db.ForeignKey(Workout_Circuit.circuit_id))
+#
+# class hasFavoriteClass(db.Model):
+#     email = db.Column(db.String(120), db.ForeignKey(User.email))
+#     class_name = db.Column(db.String(120), db.ForeignKey(Fitness_Class.name))
+#     class_location = db.Column(db.String(120), db.ForeignKey(Fitness_Class.location))
+#
+# class hasExercise(db.Model):
+#     circuit_id = db.Column(db.Integer, db.ForeignKey(Workout_Circuit.circuit_id))
+#     exercise_name = db.Column(db.String(120), db.ForeignKey(Exercise.name))
+
 
 @app.route('/', methods=['GET'])
 def index():
--- models.py
+++ models.py
@@ -1,22 +1,35 @@
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return f"User '{self.username}', '{self.email}'"
-
-class Equipment(db.Model):
-    name = db.Column(db.String(120), primary_key = True)
-
-class Exercise(db.Model):
-    name = db.Column(db.String(120), primary_key = True)
-    body_part = db.Column(db.String(120))
-
-class Workout_Circuit(db.Model):
-    circuit_id = db.Column(db.Integer, primary_key = True)
-    workout_type = db.Column(db.String(120))
-
-class Fitness_Class(db.Model):
-    name = db.Column(db.String(120), primary_key = True)
-    location = db.Column(db.String(120), primary_key = True)
+# class User(db.Model):
+#     email = db.Column(db.String(120), primary_key = True, nullable=False)
+#     name = db.Column(db.String(120))
+#     password = db.Column(db.String(120))
+#
+#     def __repr__(self):
+#         return f"User '{self.name}', '{self.email}'"
+#
+# class Equipment(db.Model):
+#     name = db.Column(db.String(120), primary_key = True)
+#
+# class Exercise(db.Model):
+#     name = db.Column(db.String(120), primary_key = True)
+#     body_part = db.Column(db.String(120))
+#
+# class Workout_Circuit(db.Model):
+#     circuit_id = db.Column(db.Integer, primary_key = True)
+#     workout_type = db.Column(db.String(120))
+#
+# class Fitness_Class(db.Model):
+#     name = db.Column(db.String(120), primary_key = True)
+#     location = db.Column(db.String(120), primary_key = True)
+#
+# class ownsWorkout(db.Model):
+#     email = db.Column(db.String(120), db.ForeignKey(User.email))
+#     circuit_id = db.Column(db.Integer, db.ForeignKey(Workout_Circuit.circuit_id))
+#
+# class hasFavoriteClass(db.Model):
+#     email = db.Column(db.String(120), db.ForeignKey(User.email))
+#     class_name = db.Column(db.String(120), db.ForeignKey(Fitness_Class.name))
+#     class_location = db.Column(db.String(120), db.ForeignKey(Fitness_Class.location))
+#
+# class hasExercise(db.Model):
+#     circuit_id = db.Column(db.Integer, db.ForeignKey(Workout_Circuit.circuit_id))
+#     exercise_name = db.Column(db.String(120), db.ForeignKey(Exercise.name))
