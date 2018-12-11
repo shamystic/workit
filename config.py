09efd53e1bdfd3aaea0a519e917772f47de87269
Hemanth Yakkali <hy115@duke.edu>
2018-12-10 20:34:58 -0500

    Updated app.py to what it should be

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ POSTGRES = {
     'pw': 'password',
     'db': 'dfx',
     'host': 'localhost',
-    'port': '5433',
+    'port': '5432',
 }
 
-SQLALCHEMY_DATABASE_URI = 'postgresql:///dfx'
-#%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+# SQLALCHEMY_DATABASE_URI = 'postgresql:///dfx'
+SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
 # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost/dfx"  # 'postgresql://shamikh3:dbpasswd@localhost/beers'
 SQLALCHEMY_ECHO = True
