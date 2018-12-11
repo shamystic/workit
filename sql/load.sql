d4cb6ee2d3d50f6f48072cc275d92651d078ef66
antgraves <antgraves23@gmail.com>
2018-12-10 20:45:14 -0500

    ad

--- sql/load.sql
+++ sql/load.sql
@@ -4,43 +4,48 @@ INSERT INTO Person VALUES
     ('shamikh@duke.edu', 'ssh50', NULL, 'password24');
 
 COPY Equipment(name)
-<<<<<<< HEAD
-<<<<<<< HEAD
-FROM '/Users/priyanakkiran/WorkItGit/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
+FROM '/Users/acgraves/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
+-- #<<<<<<< HEAD
+-- #<<<<<<< HEAD
+-- FROM '/Users/priyanakkiran/WorkItGit/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
 
---FROM '/Users/acgraves/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
 
 COPY Exercise(name, body_part, equipment)
-FROM '/Users/priyanakkiran/WorkItGit/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
-=======
-FROM '/Users/priyanakkiran/WorkItGit/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
+FROM '/Users/acgraves/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
+-- FROM '/Users/priyanakkiran/WorkItGit/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
+-- =======
+-- FROM '/Users/priyanakkiran/WorkItGit/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
+-- FROM '/mnt/c/Users/shami/Projects/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
+
+
+COPY Exercise(name, body_part, equipment)
+FROM '/Users/acgraves/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
+-- FROM '/Users/priyanakkiran/WorkItGit/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
+--FROM '/Users/hyakkali/Hemanth/CS316/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
+-- FROM '/mnt/c/Users/shami/Projects/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
+-- >>>>>>> 450a0356db5cd384153c0386741899d94edfda6f
+
+
+COPY fitness_class(name, location, goal, description, times, link)
+FROM '/Users/acgraves/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
+-- FROM '/Users/priyanakkiran/WorkItGit/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;
+--FROM '/Users/hyakkali/Hemanth/CS316/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;
+-- FROM '/mnt/c/Users/shami/Projects/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;
+-- =======
+
+-- FROM '/Users/hyakkali/Hemanth/CS316/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER; 
 -- FROM '/mnt/c/Users/shami/Projects/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
 
 COPY Exercise(name, body_part, equipment)
-FROM '/Users/priyanakkiran/WorkItGit/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
---FROM '/Users/hyakkali/Hemanth/CS316/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
--- FROM '/mnt/c/Users/shami/Projects/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
->>>>>>> 450a0356db5cd384153c0386741899d94edfda6f
-
-COPY fitness_class(name, location, goal, description, times, link)
-FROM '/Users/priyanakkiran/WorkItGit/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;
---FROM '/Users/hyakkali/Hemanth/CS316/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;
--- FROM '/mnt/c/Users/shami/Projects/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;
-=======
--- FROM '/Users/acgraves/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
--- FROM '/Users/hyakkali/Hemanth/CS316/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER; 
-FROM '/mnt/c/Users/shami/Projects/workit/sql/Equipment.csv' DELIMITER ',' CSV HEADER;
-
-COPY Exercise(name, body_part, equipment)
--- FROM '/Users/acgraves/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
+FROM '/Users/acgraves/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
 -- FROM '/Users/hyakkali/Hemanth/CS316/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER; 
-FROM '/mnt/c/Users/shami/Projects/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
+-- FROM '/mnt/c/Users/shami/Projects/workit/sql/Exercises.csv' DELIMITER ',' CSV HEADER;
 
 COPY fitness_class(name, location, goal, description, times, link)
--- FROM '/Users/acgraves/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;
+FROM '/Users/acgraves/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;
 -- FROM '/Users/hyakkali/Hemanth/CS316/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;  
-FROM '/mnt/c/Users/shami/Projects/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;
->>>>>>> d587de8ad58259ac9efa5eb5da942d22d03c7550
+-- FROM '/mnt/c/Users/shami/Projects/workit/sql/workout_classes.csv' DELIMITER ',' CSV HEADER;
+-- >>>>>>> d587de8ad58259ac9efa5eb5da942d22d03c7550
 
 INSERT INTO Workout VALUES
     ('Cardio', 'Strength & Endurance'),
