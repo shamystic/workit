d9c9b24b9a66fb9f07370f968fdca4f8caacf3ef
priya.nakkiran@duke.edu <priyanakkiran@Priyas-MacBook-Air.local>
2018-12-10 21:29:28 -0500

    Replacing with identical version

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager, login_required, login_user, logout_user, c
 # do current_user to get logged in user
 # do current_user.email to get current email
 
-app = Flask(__name__)
+app = Flask(_name_)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
@@ -24,15 +24,15 @@ def load_user(user_id):
 @app.route('/', methods=['GET'])
 def index():
     print(current_user)
-    return render_template('main.html')
+    return render_template('mainTemplate.html')
 
 @app.route('/equipment', methods = ['GET'])
 def show_equipment():
-	return render_template('equipment.html', equipment = Equipment.query.with_entities(Equipment.name))
+    return render_template('equipment.html', equipment = Equipment.query.with_entities(Equipment.name))
 
 @app.route('/exercises', methods = ['GET'])
 def show_exercises():
-	return render_template('exercises.html', exercises = Exercise.query.all())
+    return render_template('exercises.html', exercises = Exercise.query.all())
 
 @app.route('/workouts', methods = ['GET'])
 def show_workouts():
@@ -40,26 +40,43 @@ def show_workouts():
     exercises = hasExercise.query.all()
     return render_template('workouts.html', workouts = workouts, exercises = exercises)
 
-@app.route('/saved-workouts', methods = ['GET'])
-def saved_workouts():
-    workouts = ownsWorkout.query.filter_by(email = current_user.email).all()
-    exercises = hasExercise.query.all()
-    return render_template('saved-workouts.html', workouts = workouts, exercises = exercises)
+@app.route('/classes', methods = ['GET'])
+def show_classes():
+    return render_template('classes.html', classes = FitnessClass.query.all())
 
 @app.route('/create-workout', methods = ['GET', 'POST'])
+#@login_required
 def create_workout():
     if request.method == 'GET':
-        return render_template('workout-form.html', exercises = Exercise.query.all())
+        exercises = Exercise.query.all()
+        body_parts = Exercise.query.with_entities(Exercise.body_part).distinct()
+        return render_template('workout-form.html', exercises = exercises, body_parts = body_parts)
     workout_name = request.form['name']
-    workout_type = request.form['type']
+    workout_type = request.form['workout_type']
     workout = Workout(workout_id = workout_name, workout_type = workout_type)
     db.session.add(workout)
+    db.session.commit()
+    own = ownsWorkout(email = current_user.email, workout_id = workout_name, favorite = False)
+    db.session.add(own)
     db.session.commit()
     for item in request.form.getlist('workout'):
         temp = hasExercise(workout_id = workout_name, exercise_id = item)
         db.session.add(temp)
         db.session.commit()
     return redirect(url_for('create_workout'))
+
+@app.route('/add-favorite/<string:workout_name>', methods = ['GET', 'POST'])
+def add_favorite(workout_name):
+    temp = ownsWorkout(email = current_user.email, workout_id = workout_name, favorite = True)
+    db.session.add(temp)
+    db.session.commit()
+    return redirect(url_for('show_users'))
+
+@app.route('/saved-workouts', methods = ['GET'])
+def saved_workouts():
+    workouts = ownsWorkout.query.filter_by(email = current_user.email).all()
+    exercises = hasExercise.query.all()
+    return render_template('saved-workouts.html', workouts = workouts, exercises = exercises)
 
 @app.route('/users', methods = ['GET'])
 def show_users():
@@ -82,16 +99,21 @@ def login():
 def register():
     if request.method == 'GET':
         return render_template('register.html')
-    user = Person(email = request.form['email'], name = request.form['name'], password = request.form['password'])
+    email = request.form['email']
+    name = request.form['name']
+    password = request.form['password']
+    goal = request.form['goal']
+    user = Person(email = email, name = name, password = password, goal = goal)
     db.session.add(user)
     db.session.commit()
     print('User successfully registered!')
     return redirect(url_for('login'))
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(debug=True, host='0.0.0.0')
