eae7fed2130a0cd6515268e5e4c308e3b279b7c7
antgraves <antgraves23@gmail.com>
2018-12-11 11:17:26 -0500

    final

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ POSTGRES = {
     'port': '5433',
 }
 
-# SQLALCHEMY_DATABASE_URI = 'postgresql:///dfx'
-SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+SQLALCHEMY_DATABASE_URI = 'postgresql:///dfx'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 
 # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost/dfx"  # 'postgresql://shamikh3:dbpasswd@localhost/beers'
 SQLALCHEMY_ECHO = True
